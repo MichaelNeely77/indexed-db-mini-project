ff4e5f6f066f858c9f1e0d22cbcf37b40221e1ec
Michael Neely <michael@michaelneely.net>
2020-07-18 23:48:39 -0400

    Added the database

--- js/app.js
+++ js/app.js
@@ -10,3 +10,21 @@ const form = document.querySelector('form'),
     apppointements = document.querySelector('#appointments'),
     appintmentTitle  = document.querySelector('#appointment-title');
 
+document.addEventListener('DOMContentLoaded', () => {
+    // create the database
+    let AppointmentsDB = window.indexedDB.open('appointments', 1);
+
+    // if there is an error display 
+    AppointmentsDB.onerror = function() {
+        console.log('There was an error');
+    }
+    // if everything is fine assign the result to the instance
+    AppointmentsDB.onsuccess = function() {
+        console.log('Database Ready');
+
+        // Save the result
+         DB = AppointmentsDB.result;
+    }
+
+   
+});
