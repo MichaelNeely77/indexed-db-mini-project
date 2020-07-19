e9f4c87455e0edc2fecbb4402ad96462887379e4
Michael Neely <michael@michaelneely.net>
2020-07-18 23:39:58 -0400

    Added the variables for the mini-project

---
+++ js/app.js
@@ -0,0 +1,12 @@
+let DB;
+
+const form = document.querySelector('form'),
+    petName = document.querySelector('#pet-name'),
+    ownerName = document.querySelector('#owner-name'),
+    phone = document.querySelector('#phone'),
+    date = document.querySelector('#date'),
+    hour = document.querySelector('#hour'),
+    symptoms = document.querySelector('#symptoms'),
+    apppointements = document.querySelector('#appointments'),
+    appintmentTitle  = document.querySelector('#appointment-title');
+
