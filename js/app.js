4c886b3503dc8af58ebd153eed1bff1dd14e7f11
Michael Neely <michael@michaelneely.net>
2020-07-19 23:33:40 -0400

    Added the keypath fr the IndexedDB

--- js/app.js
+++ js/app.js
@@ -12,19 +12,33 @@ const form = document.querySelector('form'),
 
 document.addEventListener('DOMContentLoaded', () => {
     // create the database
-    let AppointmentsDB = window.indexedDB.open('appointments', 1);
+    let AppointmentDB = window.indexedDB.open('appointments', 1);
 
     // if there is an error display 
-    AppointmentsDB.onerror = function() {
+    AppointmentDB.onerror = function() {
         console.log('There was an error');
     }
     // if everything is fine assign the result to the instance
-    AppointmentsDB.onsuccess = function() {
-        console.log('Database Ready');
+    AppointmentDB.onsuccess = function() {
+        // console.log('Database Ready');
 
         // Save the result
-         DB = AppointmentsDB.result;
+         DB = AppointmentDB.result;
     }
 
-   
+    // This method runs once (great for the schema)
+    AppointmentDB.onupgradeneeded = function(e){
+        // The event will be the database
+        let db  = e.target.result;
+
+        // Create an objexct store
+        // Keypath is going to  be nt eh indeces
+        let objectStore = db.createObjectStore('appointments', {keyPath: 'key', autoIncrement: true } );
+
+        // create index )1) field name 2) keypath 3) options
+        objectStore.createIndex('petname', 'petname', { unique: false} );
+
+        console.log("Database ready and fields created");
+
+    }
 });
